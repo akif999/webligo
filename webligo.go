f483904db9c8572d944ad747ce217220a94ae418
AKIF999 <aki01nf28@gmail.com>
2017-07-15 06:37:01 +0900

    change variable name

--- webligo.go
+++ webligo.go
@@ -15,29 +15,29 @@ const (
 )
 
 var (
-	refWord = kingpin.Arg("refWord", "refWord string").Required().String()
+	searchWord = kingpin.Arg("searchWord", "searchWord string").Required().String()
 )
 
 func main() {
 	kingpin.Parse()
 
-	// if refWord includes multibyte charactor, set mode to JpToEn
+	// if searchWord includes multibyte charactor, set mode to JpToEn
 	mode := EnToJp
-	if utf8.RuneCountInString(*refWord) != len(*refWord) {
+	if utf8.RuneCountInString(*searchWord) != len(*searchWord) {
 		mode = JpToEn
 	}
 
-	doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + *refWord)
+	doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + *searchWord)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if mode == EnToJp {
-		fmt.Printf("単語             : %s\n", *refWord)
+		fmt.Printf("単語             : %s\n", *searchWord)
 		fmt.Printf("主な意味         : %s\n", doc.Find(".content-explanation").Text())
 		fmt.Printf("音節             : %s\n", doc.Find(".syllableEjje").Text())
 		fmt.Printf("発音記号・読み方 : %s\n", doc.Find(".phoneticEjjeDesc").Text())
 	} else {
-		fmt.Printf("日単語           : %s\n", *refWord)
+		fmt.Printf("日単語           : %s\n", *searchWord)
 		fmt.Printf("英単語           : %s\n", doc.Find(".content-explanation").Text())
 	}
 }
