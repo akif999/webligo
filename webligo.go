8b715b6948cafac6c42fd8ef1db6ee112113b399
akif999 <aki01nf28@gmail.com>
2017-11-24 23:14:44 +0900

    fix logic inversement

--- webligo.go
+++ webligo.go
@@ -21,13 +21,13 @@ func main() {
 		log.Fatal(err)
 	}
 	if isIncludeMultibyte(*searchWord) {
+		fmt.Printf("日単語           : %s\n", *searchWord)
+		fmt.Printf("英単語           : %s\n", doc.Find(".content-explanation").Text())
+	} else {
 		fmt.Printf("単語             : %s\n", *searchWord)
 		fmt.Printf("主な意味         : %s\n", doc.Find(".content-explanation").Text())
 		fmt.Printf("音節             : %s\n", doc.Find(".syllableEjje").Text())
 		fmt.Printf("発音記号・読み方 : %s\n", doc.Find(".phoneticEjjeDesc").Text())
-	} else {
-		fmt.Printf("日単語           : %s\n", *searchWord)
-		fmt.Printf("英単語           : %s\n", doc.Find(".content-explanation").Text())
 	}
 }
 
