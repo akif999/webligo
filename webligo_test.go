4cbad0f2bf5354210b63a768a3de9f0f266f9334
akif999 <aki01nf28@gmail.com>
2017-11-25 12:21:45 +0900

    add tests

---
+++ webligo_test.go
@@ -0,0 +1,42 @@
+package main
+
+import "testing"
+
+func TestIsIncludeMultibyte(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{
+			"apple",
+			false,
+		},
+		{
+			"umbrella",
+			false,
+		},
+		{
+			"----",
+			false,
+		},
+		{
+			"林檎",
+			true,
+		},
+		{
+			"かさ",
+			true,
+		},
+		{
+			"○",
+			true,
+		},
+	}
+
+	for i, test := range tests {
+		got := isIncludeMultibyte(test.input)
+		if got != test.expected {
+			t.Errorf("tests[%d] got %v, want %v", i, got, test.expected)
+		}
+	}
+}
