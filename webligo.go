005918f1f26213753cbec3ebd61aa136f92107c9
akif999 <aki01nf28@gmail.com>
2017-11-28 22:06:42 +0900

    refactoring

--- webligo.go
+++ webligo.go
@@ -10,21 +10,21 @@ import (
 )
 
 var (
-	searchWord = kingpin.Arg("searchWord", "searchWord string").Required().String()
+	word = kingpin.Arg("word", "target word").Required().String()
 )
 
 func main() {
 	kingpin.Parse()
 
-	doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + *searchWord)
+	doc, err := goquery.NewDocument("http://ejje.weblio.jp/content/" + *word)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if isIncludeMultibyte(*searchWord) {
-		fmt.Printf("日単語           : %s\n", *searchWord)
+	if isIncludeMultibyte(*word) {
+		fmt.Printf("日単語           : %s\n", *word)
 		fmt.Printf("英単語           : %s\n", doc.Find(".content-explanation").Text())
 	} else {
-		fmt.Printf("単語             : %s\n", *searchWord)
+		fmt.Printf("単語             : %s\n", *word)
 		fmt.Printf("主な意味         : %s\n", doc.Find(".content-explanation").Text())
 		fmt.Printf("音節             : %s\n", doc.Find(".syllableEjje").Text())
 		fmt.Printf("発音記号・読み方 : %s\n", doc.Find(".phoneticEjjeDesc").Text())
@@ -32,8 +32,5 @@ func main() {
 }
 
 func isIncludeMultibyte(str string) bool {
-	if utf8.RuneCountInString(str) != len(str) {
-		return true
-	}
-	return false
+	return utf8.RuneCountInString(str) != len(str)
 }
